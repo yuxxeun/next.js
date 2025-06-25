3f1466189aa47c0453297477feed160326c0f373
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-06-26 04:33:19 +0700

    wip

--- src/lib/primitive.ts
+++ src/lib/primitive.ts
@@ -2,29 +2,12 @@
 
 import { composeRenderProps } from "react-aria-components"
 import { twMerge } from "tailwind-merge"
-import { tv } from "tailwind-variants"
 
 function composeTailwindRenderProps<T>(
   className: string | ((v: T) => string) | undefined,
-  tailwind: string,
+  tailwind: string | string[],
 ): string | ((v: T) => string) {
   return composeRenderProps(className, (className) => twMerge(tailwind, className))
 }
 
-const focusRing = tv({
-  variants: {
-    isFocused: { true: "outline-hidden ring-4 ring-ring/20 data-invalid:ring-danger/20" },
-    isFocusVisible: { true: "outline-hidden ring-4 ring-ring/20" },
-    isInvalid: { true: "ring-4 ring-danger/20" },
-  },
-})
-
-const focusStyles = tv({
-  extend: focusRing,
-  variants: {
-    isFocused: { true: "border-ring/70 forced-colors:border-[Highlight]" },
-    isInvalid: { true: "border-danger/70 forced-colors:border-[Mark]" },
-  },
-})
-
-export { composeTailwindRenderProps, focusRing, focusStyles }
+export { composeTailwindRenderProps }
