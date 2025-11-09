c68c21d4a59b3942638b4157156c123b441b3af0
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-11-09 23:43:23 +0700

    wip

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { useTheme } from "@/components/theme-provider"
+import { useTheme } from "next-themes"
 import { Toaster as ToasterPrimitive, type ToasterProps } from "sonner"
+import { twJoin } from "tailwind-merge"
 
-const Toast = ({ ...props }: ToasterProps) => {
+export function Toast(props: ToasterProps) {
   const { theme = "system" } = useTheme()
   return (
     <ToasterPrimitive
@@ -11,8 +12,10 @@ const Toast = ({ ...props }: ToasterProps) => {
       className="toaster group"
       richColors
       toastOptions={{
-        className:
-          "*:data-icon:self-start font-sans has-data-description:*:data-icon:mt-1 *:data-icon:mt-0.5 backdrop-blur-2xl",
+        className: twJoin(
+          "not-has-data-[slot=note]:backdrop-blur-3xl *:data-icon:mt-0.5 *:data-icon:self-start has-data-description:*:data-icon:mt-1",
+          "**:data-action:[--normal-bg:var(--color-primary-fg)] **:data-action:[--normal-text:var(--color-primary)]",
+        ),
       }}
       style={
         {
@@ -20,27 +23,24 @@ const Toast = ({ ...props }: ToasterProps) => {
           "--normal-text": "var(--color-overlay-fg)",
           "--normal-border": "var(--color-border)",
 
-          "--success-bg": "var(--color-success-bg)",
-          "--success-border": "var(--color-success-border)",
-          "--success-text": "var(--color-success-text)",
+          "--success-bg": "var(--color-success-subtle)",
+          "--success-border": "color-mix(in oklab, var(--success-subtle-fg) 20%, transparent)",
+          "--success-text": "var(--color-success-subtle-fg)",
 
-          "--error-bg": "var(--color-error-bg)",
-          "--error-border": "var(--color-error-border)",
-          "--error-text": "var(--color-error-text)",
+          "--error-bg": "var(--color-danger-subtle)",
+          "--error-border": "color-mix(in oklab, var(--danger-subtle-fg) 20%, transparent)",
+          "--error-text": "var(--color-danger-subtle-fg)",
 
-          "--warning-bg": "var(--color-warning-bg)",
-          "--warning-border": "var(--color-warning-border)",
-          "--warning-text": "var(--color-warning-text)",
+          "--warning-bg": "var(--color-warning-subtle)",
+          "--warning-border": "color-mix(in oklab, var(--warning-subtle-fg) 20%, transparent)",
+          "--warning-text": "var(--color-warning-subtle-fg)",
 
-          "--info-bg": "var(--color-info-bg)",
-          "--info-border": "var(--color-info-border)",
-          "--info-text": "var(--color-info-text)",
+          "--info-bg": "var(--color-info-subtle)",
+          "--info-border": "color-mix(in oklab, var(--info-subtle-fg) 20%, transparent)",
+          "--info-text": "var(--color-info-subtle-fg)",
         } as React.CSSProperties
       }
       {...props}
     />
   )
 }
-
-export type { ToasterProps }
-export { Toast }
